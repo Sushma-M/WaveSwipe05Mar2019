1fad59a25cda2e419c60170a01bfe5f0bf57cca5
Sushma Maddirala <sushma.maddirala@wavemaker.com>
2019-03-04 09:12:00 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,12 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "mobile",
+  "defaultLanguage" : "en",
+  "displayName" : "WaveSwipe",
+  "homePage" : "Main",
+  "name" : "WaveSwipe",
+  "platformType" : "MOBILE",
+  "securityEnabled" : "false",
+  "supportedLanguages" : "en",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
